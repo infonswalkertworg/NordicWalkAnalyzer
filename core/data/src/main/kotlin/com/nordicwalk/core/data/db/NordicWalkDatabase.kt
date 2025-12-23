e7c784a97624f1e2955f9e56c4d1577baa50ae3a
infonswalkertworg <info.nswalkertw.org@gmail.com>
2025-12-23 17:48:36 +0800

    Room database initialization and schema definition

---
+++ core/data/src/main/kotlin/com/nordicwalk/core/data/db/NordicWalkDatabase.kt
@@ -0,0 +1,45 @@
+package com.nordicwalk.core.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.nordicwalk.core.data.db.converter.DateTimeConverters
+import com.nordicwalk.core.data.db.dao.StudentDao
+import com.nordicwalk.core.data.db.dao.TrainingRecordDao
+import com.nordicwalk.core.data.db.dao.AnalysisSessionDao
+import com.nordicwalk.core.data.db.entity.StudentEntity
+import com.nordicwalk.core.data.db.entity.TrainingRecordEntity
+import com.nordicwalk.core.data.db.entity.AnalysisSessionEntity
+
+@Database(
+    entities = [
+        StudentEntity::class,
+        TrainingRecordEntity::class,
+        AnalysisSessionEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(DateTimeConverters::class)
+abstract class NordicWalkDatabase : RoomDatabase() {
+    abstract fun studentDao(): StudentDao
+    abstract fun trainingRecordDao(): TrainingRecordDao
+    abstract fun analysisSessionDao(): AnalysisSessionDao
+
+    companion object {
+        private const val DATABASE_NAME = "nordic_walk_db"
+        private var instance: NordicWalkDatabase? = null
+
+        fun getInstance(context: Context): NordicWalkDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    NordicWalkDatabase::class.java,
+                    DATABASE_NAME
+                ).build().also { instance = it }
+            }
+        }
+    }
+}
