2d55c55c1c589879d5d6fd3d19043eeafc7dab9a
infonswalkertworg <info.nswalkertw.org@gmail.com>
2025-12-23 17:47:05 +0800

    Initial project-level gradle configuration with dependency management

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+}
+
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
+
+tasks.register("clean") {
+    delete(rootProject.buildDir)
+}
