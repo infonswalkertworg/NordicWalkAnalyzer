826740038b578792e3d3cd1d8a2f11d6510c8e3a
infonswalkertworg <info.nswalkertw.org@gmail.com>
2025-12-25 16:25:29 +0800

    Fix navigation import path in MainActivity

--- app/src/main/kotlin/com/nordicwalk/analyzer/MainActivity.kt
+++ app/src/main/kotlin/com/nordicwalk/analyzer/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.nordicwalk.analyzer.ui.NordicWalkNavigation
+import com.nordicwalk.analyzer.ui.navigation.NordicWalkNavigation
 import com.nordicwalk.analyzer.ui.theme.NordicWalkTheme
 import dagger.hilt.android.AndroidEntryPoint
 
