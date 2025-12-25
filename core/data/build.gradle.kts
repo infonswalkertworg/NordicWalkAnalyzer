ae09b169eb6ac093d3542d57e737b10af4270cd2
infonswalkertworg <info.nswalkertw.org@gmail.com>
2025-12-25 16:10:25 +0800

    Add Hilt and kotlinx-datetime dependencies to core:data

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -30,6 +31,10 @@ dependencies {
     // Core Android
     implementation("androidx.core:core-ktx:1.12.0")
     
+    // Hilt for dependency injection
+    implementation("com.google.dagger:hilt-android:2.50")
+    ksp("com.google.dagger:hilt-compiler:2.50")
+    
     // Room Database
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
@@ -38,6 +43,9 @@ dependencies {
     // Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    
+    // Kotlin Datetime
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
     
     // Testing
     testImplementation("junit:junit:4.13.2")
